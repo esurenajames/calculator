d0bf0ed6f8e66048a1034345a070fb747174723d
James Esurena <84180003+esurenajames@users.noreply.github.com>
2023-11-27 21:59:13 +0800

    Update script.js
    Attempt to reduce redundancy by using one function only.

--- calculator/script.js
+++ calculator/script.js
@@ -1,4 +1,4 @@
-let calculation = '';
+        let calculation = '';
 
         function updateResult() {
         let result = '';
@@ -14,110 +14,96 @@ let calculation = '';
             updateResult();
         }
 
-        function button1(){
-            calculation += '1'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('1');
-        }
+        function button(buttonpressed){
 
-        function button2(){
-            calculation += '2'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('2');
-        }
-
-        function button3(){
-            calculation += '3'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('3');
-        }
-
-        function button4(){
-            calculation += '4'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('4');
-        }
-
-        function button5(){
-            calculation += '5'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('5');
-        }
-
-        function button6(){
-            calculation += '6'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('6');
-        }
-
-        function button7(){
-            calculation += '7'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('7');
-        }
-
-        function button8(){
-            calculation += '8'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('8');
-        }
-
-        function button9(){
-            calculation += '9'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('9');
-        }
-
-        function button0(){
-            calculation += '0'
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('0');
-        }
-
-        function plusButton(){
-            calculation += ' \+ '
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked(' + ');
-        }
-
-        function minusButton(){
-            calculation += ' \- '
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked(' - ');
-        }
-
-        function multiplyButton(){
-            calculation += ' * '
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked(' * ');
-        }
-
-        function divideButton(){
-            calculation += ' / '
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked(' / ');
-        }
-
-        function equalButton(){
-            eval(calculation);
-            calculation = eval(calculation);
-            document.getElementById('dito').innerHTML = calculation;   
-        }
-
-        function resetButton(){
-            calculation = '';
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('');
-        }
-
-        function decimalButton(){
-            calculation = `${calculation + '.'}`;
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked('.');
-        }
-
-        function percentButton(){
-            calculation = `${calculation / 100}`;
-            document.getElementById('dito').innerHTML = calculation;
-            buttonClicked(`${calculation / 100}`);
+            if(buttonpressed === 'one'){
+                calculation += '1'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('1');
+            }
+            if(buttonpressed === 'two'){
+                calculation += '2'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('2');
+            }
+            if(buttonpressed === 'three'){
+                calculation += '3'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('3');
+            }
+            if(buttonpressed === 'four'){
+                calculation += '4'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('4');
+            }
+            if(buttonpressed === 'five'){
+                calculation += '5'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('5');
+            }
+            if(buttonpressed === 'six'){
+                calculation += '6'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('6');
+            }
+            if(buttonpressed === 'seven'){
+                calculation += '7'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('7');
+            }
+            if(buttonpressed === 'eight'){
+                calculation += '8'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('8');
+            }
+            if(buttonpressed === 'nine'){
+                calculation += '9'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('9');
+            }
+            if(buttonpressed === 'zero'){
+                calculation += '0'
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('0');
+            }
+            if(buttonpressed === 'plus'){
+                calculation += ' \+ '
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked(' + ');
+            }
+            if(buttonpressed === 'minus'){
+                calculation += ' \- '
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked(' - ');
+            }
+            if(buttonpressed === 'divide'){
+                calculation += ' / '
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked(' / ');
+            }
+            if(buttonpressed === 'multiply'){
+                calculation += ' * '
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked(' * ');
+            }
+            if(buttonpressed === 'equal'){
+                eval(calculation);
+                calculation = eval(calculation);
+                document.getElementById('dito').innerHTML = calculation;   
+            }
+            if(buttonpressed === 'reset'){
+                calculation = '';
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('');
+            }
+            if(buttonpressed === 'decimal'){
+                calculation = `${calculation + '.'}`;
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked('.');
+            }
+            if(buttonpressed === 'percent'){
+                calculation = `${calculation / 100}`;
+                document.getElementById('dito').innerHTML = calculation;
+                buttonClicked(`${calculation / 100}`);
+            }
         }
